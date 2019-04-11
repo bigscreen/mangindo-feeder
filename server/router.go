90a3aabdff9cdfbafd86d079edd15e535904cb19
Gallant Pratama <gallant.pratama@go-jek.com>
2019-04-11 10:22:56 +0700

    Add spec to show ping api in handler

--- server/router.go
+++ server/router.go
@@ -9,6 +9,7 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/ping", handler.PingHandler).Methods("GET")
 	router.NotFoundHandler = http.HandlerFunc(handler.NotFoundHandler)
 
 	return router
