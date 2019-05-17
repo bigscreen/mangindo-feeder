dae1776f27c2cd719e230f73e8099a8c26a60429
Gallant Pratama <gallant.pratama@go-jek.com>
2019-05-17 13:25:51 +0700

    Add test to read base url config

--- config/config_test.go
+++ config/config_test.go
@@ -8,15 +8,16 @@ import (
 
 func TestConfig(t *testing.T) {
 	configVars := map[string]string{
-		"APP_PORT":             "3001",
-		"LOG_LEVEL":            "debug",
-		"ENVIRONMENT":          "test",
-		"REDIS_HOST":           "localhost",
-		"REDIS_PORT":           "6379",
-		"REDIS_POOL":           "10",
-		"WORKER_REDIS_ADDRESS": "127.0.0.1:6379",
-		"POPULAR_MANGA_TAGS":   "foo1, foo2",
-		"ADS_CONTENT_TAGS":     "foo1, foo2",
+		"APP_PORT":               "3001",
+		"LOG_LEVEL":              "debug",
+		"ENVIRONMENT":            "test",
+		"REDIS_HOST":             "localhost",
+		"REDIS_PORT":             "6379",
+		"REDIS_POOL":             "10",
+		"WORKER_REDIS_ADDRESS":   "127.0.0.1:6379",
+		"ORIGIN_SERVER_BASE_URL": "https://foo.com",
+		"POPULAR_MANGA_TAGS":     "foo1, foo2",
+		"ADS_CONTENT_TAGS":       "foo1, foo2",
 	}
 
 	for k, v := range configVars {
@@ -32,6 +33,7 @@ func TestConfig(t *testing.T) {
 	assert.Equal(t, 6379, RedisPort())
 	assert.Equal(t, 10, RedisPool())
 	assert.Equal(t, configVars["WORKER_REDIS_ADDRESS"], WorkerRedisAddress())
+	assert.Equal(t, configVars["ORIGIN_SERVER_BASE_URL"], BaseURL())
 	assert.Equal(t, []string{"foo1", "foo2"}, PopularMangaTags())
 	assert.Equal(t, []string{"foo1", "foo2"}, AdsContentTags())
 }
